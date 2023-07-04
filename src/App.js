95960c80704af6fbe02f089725f5b5f8fc37ad74
Deepak Kumar Saini <kumarsainideepak32@gmail.com>
2023-07-04 19:06:32 +0530

    created form to submit feedback

--- src/App.js
+++ src/App.js
@@ -1,104 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
-import { useState } from 'react';
-
-
+import ThankYou from './thankYou';
 
 function App() {
-
-  let [submitted, setSubmitted] = useState(false);
-
-  const inputStyle = {
-    border: '1px solid #E5DBC6',
-    padding: '20px',
-    borderRadius: '10px',
-    width: '80%',
-    backgroundColor: '#265742',
-    color: '#E5DBC6',
-  };
-
-  const formStyle = {
-    backgroundColor: '#265742',
-    display: 'flex',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    flexDirection: 'column',
-    padding: '50px'
-  };
-
-  const submitStyle = {
-    // border:'1px solid #E5DBC6',
-    padding: '20px',
-    borderRadius: '10px',
-    color: 'black',
-    backgroundColor: '#9EDC72',
-    width: '100%',
-    textAlignment: 'center'
-  };
-
-  const labelStyle = {
-    color: '#ACBBB4',
-    // fontSize: '24px',
+  const rating = new window.URLSearchParams(window.location.search).get('value');
+  if (!localStorage.getItem('rating')) {
+    axios.post('http://localhost:8000/email/rating', {
+    rating
+  })
+  .then(res => {
+    localStorage.setItem('rating', true);
+  })
+  .catch(console.log);
   }
 
-  const legendStyle = {
-    color: '#ACBBB4',
-    // fontSize: '36px',
-    width: '30%',
+  const sendFeedback = () => {
+    const feedback = document.getElementById('feedback').value;
+      
+    axios.post('http://localhost:8000/email/feedback', {
+      feedback
+    })
+    .then(res => {
+      document.getElementById('main').remove();
+      console.log('After success');
+      const h2 = document.createElement('h2')
+      h2.textContent = 'Thanks, we really appreciate your feedback.';
+      h2.className = 'thank-you';
+      document.getElementById('root').appendChild(h2);
+      localStorage.setItem('feedback', true);
+    })
+    .catch(console.log);
   }
 
-  const headerStyle = {
-    backgroundColor: '#265742',
-    color: '#ACBBB4',
-    textAlign: 'center',
+  if(!!localStorage.getItem('feedback')) {
+    return (<ThankYou/>);
+  } else {
+    return (
+      <>
+        <header id="header">
+          <img src="https://dcx14qs33eg2z.cloudfront.net/uploads/production/logos/8035546a-1af1-45a6-a671-dc0ae73b4724/resized_Logo_1_2-to-1_aspect_ratio.png" alt="Gnome" width="250px" height="65px"/>
+        </header>
+        <main id="main">
+          <h2 className='h2'>Thank you! Is there anything else you'd like to <br/> share with the gnomes?</h2>
+          {/* <h2 className='h2'>share with gnomes?</h2> */}
+  
+          <textarea id="feedback" cols={60} rows={7} >
+          </textarea>
+          <button id="button" onClick={() => sendFeedback()}>Submit</button>
+        </main>
+      </>
+    );
   }
-
-  const fieldsetStyle = {
-    padding: '60px',
-  }
-
-  return (
-    <>
-      <h1 style={headerStyle}>Your Feedback Matters...</h1>
-      <form style={formStyle}>
-        <fieldset style={fieldsetStyle}>
-          <legend style={legendStyle}>Feedback</legend>
-          <label htmlFor="fname" style={labelStyle}>First name:</label><br />
-          <input type="text" id="fname" name="fname" style={inputStyle} required={true} /><br />
-          <label htmlFor="lname" style={labelStyle}>Last name:</label><br />
-          <input type="text" id="lname" name="lname" style={inputStyle} required={true} /><br /><br />
-          <a href="http://localhost:3000/">
-            <button type="submit" onClick={() => {
-              setSubmitted(() => true);
-              const fname = document.getElementById('fname').value;
-              const lname = document.getElementById('lname').value;
-
-              if (!!fname && !!lname) {
-                axios.post('http://localhost:8000/email/verify', {
-                  fname,
-                  lname
-                })
-                  .then(res => {
-                    window.location.reload();
-                  })
-                  .catch(
-                    console.log
-                  )
-              }
-            }} style={submitStyle}>Submit</button>
-            {/* {{
-              if(submitted) {
-                <>
-                  <p>Thank You For your Feedback!!!</p>
-                </>
-              }
-            }} */}
-          </a>
-        </fieldset>
-      </form>
-    </>
-  );
 }
 
 export default App;
---
+++ src/thankYou.js
@@ -0,0 +1,13 @@
+import './App.css';
+const ThankYou = () => {
+  return (
+    <>
+      <header id="header">
+        <img src="https://dcx14qs33eg2z.cloudfront.net/uploads/production/logos/8035546a-1af1-45a6-a671-dc0ae73b4724/resized_Logo_1_2-to-1_aspect_ratio.png" alt="Gnome" width="250px" height="80px"/>
+      </header>
+      <h2 className='thank-you'>Thanks, we really appreciate your feedback.</h2>
+    </>
+  )
+}
+
+export default ThankYou;
